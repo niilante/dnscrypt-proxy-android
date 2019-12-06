62e21a87fbc693690782604ce183119546810a04
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2019-12-06 15:20:56 +0100

    Point to dns redirect

--- common/service.sh
+++ common/service.sh
@@ -11,9 +11,10 @@ MODDIR=${0%/*}
 # Check, every 15 seconds. whether an instance of dnscrypt-proxy is running,
 # if not, keep looping till network is available & start dnscrypt-proxy.
 
-	while ! [ `pgrep -x dnscrypt-proxy` ] ; do
-		$MODDIR/system/bin/dnscrypt-proxy -config  /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml && sleep 15;
-	done
+	#while ! [ `pgrep -x dnscrypt-proxy` ] ; do
+		#$MODDIR/system/bin/dnscrypt-proxy -config  /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml && sleep 15;
+		$MODDIR/system/bin/dnscrypt-proxy -config  /data/media/0/dnscrypt-proxy/dnscrypt-proxy.toml
+	#done
 
 #IPTABLES
 # Let dnscrypt-proxy do its job only. Let the user decide how to do the "redirection of dns request".
